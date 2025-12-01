e3ad63fbfa6323b5cce389aeb3a1924d4969534c
Davi Rodrigues Dias <daviddias07032006@gmail.com>
2025-12-01 10:13:07 -0300

    Teste script

---
+++ scripts/register.js
@@ -0,0 +1,13 @@
+
+
+function RegisterYou() {
+    
+    const yourname = document.body.querySelector("#name")
+const yourphone = document.body.querySelector("#tel")
+const youremail = document.body.querySelector("#email")
+const youraddres = document.body.querySelector("#addres")
+const yourdate = document.body.querySelector("#date")
+document.writeln(yourname.value)
+
+}
+
