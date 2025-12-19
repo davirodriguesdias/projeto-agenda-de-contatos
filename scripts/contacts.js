0c4c9522be7eaf60dce7199722fa217810f2f932
Davi Rodrigues Dias <daviddias07032006@gmail.com>
2025-12-18 23:39:50 -0300

    modal contacts

--- scripts/contacts.js
+++ scripts/contacts.js
@@ -1,7 +1,9 @@
 
+
 const divname = document.body.querySelector("#divname")
 const divnumber = document.body.querySelector("#divnumber")
 const userimg = document.body.querySelector("#userimage")
+const addcontact = document.body.querySelector("#add")
 
 profile = sessionStorage.getItem("userimg")
 
@@ -19,3 +21,22 @@ if(!profile) {
 else {
     userimg.src = profile
 }
+
+addcontact.addEventListener("click", function(){
+    // title = document.createElement("h2")
+    // title = document.createElement("img")
+    // uploadimg = document.createElement("input")
+    // nameinput = document.createElement("input")
+    // addresinput = document.createElement("input")
+    // emailinput = document.createElement("input")
+    // datainput = document.createElement("input")
+    // submitinput = document.createElement("input")
+    // form = document.createElement("form")
+    const modalback = document.createElement("div") //fundo do modal
+    modalback.classList.add("modalback")
+    modal = document.createElement("div") //modal
+    modal.classList.add("modal")
+    document.body.appendChild(modalback)
+    modalback.appendChild(modal)
+
+})
